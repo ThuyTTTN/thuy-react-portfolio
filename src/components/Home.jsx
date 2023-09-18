9dead5c4b7540f18a5dd5fbe2ae02dc347eba189
Thuy Nguyen <thuynguyen@Thuys-MBP.attlocal.net>
2023-09-18 09:30:20 -0400

    wip: Home page text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import TrackVisibility from "react-on-screen";
 
-const Text = styled.div`
+const Title = styled.div`
   font-size: 1rem;
   color: #eae9e5;
 `;
 
 const Home = () => {
-  return <Text>Hello this is Home page</Text>;
+  return;
+  <>
+    <Title>Welcome to My Portfolio</Title>
+    <div>Hello, My name is</div>
+    <div>Thuy Nguyen</div>
+    <div>
+      className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software
+      Developer", "Software Engineer"]' ><span className="wrap">{text}</span>
+    </div>
+  </>;
 };
 
 export default Home;
