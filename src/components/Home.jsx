ed9fb8d92114568a24f6b0ee6d93d4496386d902
Thuy Nguyen <thuynguyen@Thuys-MBP.attlocal.net>
2023-09-18 09:16:28 -0400

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Text = styled.div`
+  font-size: 1rem;
+  color: #eae9e5;
+`;
+
+const Home = () => {
+  return <Text>Hello this is Home page</Text>;
+};
+
+export default Home;
