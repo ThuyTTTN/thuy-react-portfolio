b0708e9baaa6595f8524c2cfeeba51c408b92774
Thuy Nguyen <thuynguyen@Thuys-MBP.attlocal.net>
2023-09-21 14:18:20 -0400

    fix home page

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
     <Container>
       {/* <div className="App">Hello</div> */}
       <NavBar />
+      <Home />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
