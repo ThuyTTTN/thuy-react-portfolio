cb3b379496386d07bdbaeae0e0984123976e688f
Thuy Nguyen <thuynguyen@Thuys-MBP.attlocal.net>
2023-09-28 13:31:53 -0400

    update resume link

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -100,7 +100,7 @@ const Resume = () => {
     <Container>
       <Title>Resume </Title>
       <ButtonWrapper
-        href="https://1drv.ms/w/s!Aka643mjyMAQpQaZPaW1evtd59rc?e=2Rphh7"
+        href="https://1drv.ms/w/s!Aka643mjyMAQpQaZPaW1evtd59rc?e=zIy3mv"
         target="_blank"
         rel="noopener noreferrer"
         variant="light"
